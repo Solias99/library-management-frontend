e6a7d21ec1f52c33f77559042855f77f9a8c0d6a
Rohan Mayya <mayya.rohan@gmail.com>
2019-05-20 10:45:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    axios.get("http://localhost:3000/api/v1/books").then(response => console.log(response));
+  }
+  
+  render() {
+    return(
+      <Fragment>
+        <div>Hello</div>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
